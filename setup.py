e08622c4f67582658d524743c531e6f7866d1be5
Guilherme Carraro Martins <guicarraromar@outlook.com>
2019-05-08 12:56:24 -0400

    Packaging

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kcleaner',
+    version='0.1.3',
+    packages=find_packages(),
+    include_package_data=True,
+    py_modules=['kcleaner'],
+    install_requires=[
+        'Click',
+        'iterfzf',
+        'pyyaml'
+    ],
+    entry_points='''
+        [console_scripts]
+        kcleaner=kcleaner:main
+    ''',
+)
