b8bf1a0f038e4208e4e991063dae33f80da8668f
Guilherme Carraro Martins <guicarraromar@outlook.com>
2019-05-08 12:56:19 -0400

    Renaming script

---
+++ kcleaner.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3.7
+import click
+import yaml
+from pathlib import Path
+from iterfzf import iterfzf
+
+
+def ask_yn(yn_question, default='n'):
+    tries = 0
+    while True:
+        response = input(f"{yn_question}(y/n)")
+        tries = tries + 1
+        if response in  ['y', 'n']:
+            break
+        elif tries > 2:
+            response = default
+            break
+    return response
+
+def update_file(filename, yamldoc):
+    with open(filename, 'w') as stream:
+        try:
+            yaml.dump(yamldoc, stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+def get_file(filename):
+    with open(filename, 'r') as stream:
+        try:
+            config_file = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return config_file
+
+def remove_resource(config_file, removing_type):
+    resources_name_list = []
+    for resource in config_file[removing_type]:
+        resources_name_list.append(resource['name'])
+
+    resources_to_remove = []
+    resources_to_remove = (iterfzf(resources_name_list, multi=True))
+    if resources_to_remove == None:
+        print("No resources to remove selected!")
+        exit()
+
+    print(resources_to_remove)
+    print(f"{len(config_file[removing_type])} {removing_type} in the end")
+
+    response = ask_yn("Remove Related Resources?")
+    print(f"Your response = {response}")
+
+    try:
+        config_file[removing_type] = [item for item in config_file[removing_type] if item['name'] not in resources_to_remove]
+    except KeyError:
+        print(f"Something went wrong!!")
+
+    print(f"{len(config_file[removing_type])} {removing_type} in the end")
+
+    return config_file
+
+
+@click.command()
+@click.option('--module', type=click.Choice(['user', 'cluster', 'context']), default='cluster')
+@click.option('--kubeconfig', default=f'{Path.home()}/Source/kubeconfig-cleaner-cli/config')
+@click.option(
+    '--name', '-n',
+    help='Name of the entry to remove',
+)
+def main(module, name, kubeconfig):
+    """
+    A little CLI tool to help keeping our Config Files clean :)
+    """
+    config_file = get_file(kubeconfig)
+
+    if module == 'cluster':
+        removing_type = 'clusters'
+    elif module == 'user':
+        removing_type = 'users'
+    elif module == 'context':
+        removing_type = 'contexts'
+
+    config_file = remove_resource(config_file, removing_type)
+    
+
+    update_file("./config", kubeconfig)
