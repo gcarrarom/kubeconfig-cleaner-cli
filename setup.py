eecb5edb101da0d659fe3a0ecfe3cef1fe314154
Guilherme Carraro Martins <guicarraromar@outlook.com>
2019-05-09 07:33:02 -0400

    Setting the version to the naming scheme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kcleaner',
-    version='0.0.beta-4',
+    version='0.0.5',
     packages=find_packages(),
     include_package_data=True,
     py_modules=['kcleaner'],
