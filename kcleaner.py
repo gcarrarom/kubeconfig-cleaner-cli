34c51c53b8c268373202b18692b1f42383d701a0
Guilherme Carraro Martins <guicarraromar@outlook.com>
2019-05-27 14:59:16 -0400

    Changed the way to view logs and exceptions

--- kcleaner.py
+++ kcleaner.py
@@ -26,7 +26,7 @@ def update_file(filename, yamldoc):
             logging.debug("Dumping new yaml doc into the config file")
             yaml.dump(yamldoc, stream)
         except yaml.YAMLError as exc:
-            print(exc)
+            logging.exception("Exception occured while trying to write Yaml file")
 
 def get_file(filename):
     logging.debug(f'Trying to retrieve contents of file {filename}')
@@ -35,14 +35,14 @@ def get_file(filename):
         try:
             config_file = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
-            print(exc)
+            logging.exception("Exception occured while trying to load Yaml file")
     logging.debug(f'File Contents\n{config_file}')
     logging.debug(f'Type of the file contents: {type(config_file)}')
     if config_file == None:
-        print("Config File is empty! Can't use it.")
+        logging.error("Config File is empty! Can't use it.")
         exit(11)
     elif type(config_file) == str:
-        print("Config File is not a valid yaml file!")
+        logging.error("Config File is not a valid yaml file!")
         exit(12)
     return config_file
 
@@ -52,7 +52,7 @@ def test_file_exists(filename):
     if exists:
         logging.debug("File exists!")
     else:
-        print('Config File Not found!')
+        logging.error('Config File Not found!')
         # Keep presets
         exit(10)
 
@@ -68,7 +68,7 @@ def remove_resource(config_file, removing_type):
     resources_to_remove = (iterfzf(resources_name_list, multi=True))
     logging.debug('List of resources selected: {resources_to_remove}')
     if resources_to_remove == None:
-        print("No resources to remove selected!")
+        logging.error("No resources to remove selected!")
         exit()
 
     logging.debug(f"{len(config_file[removing_type])} {removing_type} before the removal")
@@ -81,12 +81,14 @@ def remove_resource(config_file, removing_type):
         logging.debug('Removing resources...')
         config_file[removing_type] = [item for item in config_file[removing_type] if item['name'] not in resources_to_remove]
     except KeyError:
-        print(f"Something went wrong!!")
+        logging.exception(f"Something went wrong!!")
 
     logging.debug(f"{len(config_file[removing_type])} {removing_type} in the end")
 
     return config_file
 
+def write_file(filename, yamldoc):
+    logging.info("Not Implemented Yet")
 
 @click.command()
 @click.argument(
@@ -101,8 +103,9 @@ def remove_resource(config_file, removing_type):
     default='contexts'
 )
 @click.option(
-    '--kubeconfig', '-k', default=f'{Path.home()}/.kube/config'
-    )
+    '--kubeconfig', '-k', default=f'{Path.home()}/.kube/config',
+    help="path to the config file to clean"
+)
 @click.option(
     '--name', '-n',
     help='Name of the entry to remove',
@@ -112,29 +115,38 @@ def remove_resource(config_file, removing_type):
     help='Use this to roll back latest changes',
     is_flag=True
 )
-def cli(resource, name, kubeconfig, undo):
+@click.option(
+    '--debug', '-d',
+    help='Use this to see debug level messages',
+    is_flag=True
+)
+def cli(resource, name, kubeconfig, undo, debug):
     """
     A little CLI tool to help keeping Config Files clean :)
     """
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    if debug:
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+    else:
+        logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
+
     kubeconfig_backup = f"{kubeconfig}.bak"
 
     if undo:
-        logging.debug(f"Undo flag was set! checking for the backup file...")
+        logging.info(f"Undo flag was set! checking for the backup file...")
         print(f'Searching for backup config file {kubeconfig_backup}')
         config_file = get_file(kubeconfig_backup)
     else:
         config_file = get_file(kubeconfig)
-        logging.debug(f'Backing up config file at {kubeconfig_backup} before doing anything')
+        logging.info(f'Backing up config file at {kubeconfig_backup} before doing anything')
         update_file(kubeconfig_backup, config_file)
         config_file = remove_resource(config_file, resource)
         
-    logging.debug(f'Using resource {resource}')
+    logging.info(f'Using resource {resource}')
     logging.debug(f'Config file to use: {kubeconfig}')
     if name == None:
-        logging.debug(f'Name is empty, using fzf to search for the resource to remove')
+        logging.info(f'Name is empty, using fzf to search for the resource to remove')
     else:
-        logging.debug(f'Name of the resource requested to remove: {name}')
+        logging.info(f'Name of the resource requested to remove: {name}')
 
     update_file(kubeconfig, config_file)
 
